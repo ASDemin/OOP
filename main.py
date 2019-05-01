97043aed85f613948371e33806d72d5e18d440e5
classic32 <who1777@bk.ru>
2019-05-02 00:42:24 +0300

    Вроде разобрался с первыми 6 пунктами, но дальше нужна помощь: если по сути задание у нас есть слова первое (Привет, существительное) второе(всем, существительное)
    и  мы склеиваем по первое.текст, второе.текст или  надо было
    Привет(первое, сущ)
    всем(второе, сущ.)

---
+++ main.py
@@ -0,0 +1,41 @@
+'''
+1. Создайте класс Word.
+2. Добавьте свойства text и part of speech.
+3. Добавьте возможность создавать объект слово со значениями в скобках.
+4. Создайте класс Sentence
+5. Добавьте свойство content, равное списку, состоящему из номеров слов, входящих в предложение.
+6. Добавьте метод show, составляющий предложение.
+7. Добавьте метод show_parts, отображающий, какие части речи входят в предложение.
+'''
+import re
+
+class Word:
+    text = ' '
+    part_of_speech = ' '
+
+    def __init__(self,text,part_of_speech):
+        self.text = text
+        self.part_of_speech = part_of_speech
+
+class Sentence:
+    content = [i for i in range(1, (len(re.findall('\w+', sentence))+1))]
+
+    def show(self,**sentence):
+        for key, value in sentence.items():
+            key=list.append(key.text)
+            value=list.append(value.text)
+        sentence = " ".join(list)
+        print(sentence)
+
+    def show_parts(self,**parts):
+        for key, value in parts.items():
+            key = list.append(key.part_of_speech)
+            value = list.append(key.part_of_speech)
+        print (list(set1))
+
+f = Word ('Hello','noun')
+s = Word ('my','pronoun')
+t = Word('is', 'preposition')
+f=Word('Alex','noun')
+
+
